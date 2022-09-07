03d047738e0ffd27f43d7c3cef7295de86ffa15a
Mauricio Sevilla <jmsevillam@unal.edu.co>
2022-09-07 18:41:37 +0200

    python3 printing added

--- MPI/Basics_python/first.py
+++ MPI/Basics_python/first.py
@@ -1,4 +1,6 @@
 from mpi4py import MPI 
 
-rank = MPI.COMM_WORLD.Get_rank()
-print(rank)
+my_rank = MPI.COMM_WORLD.Get_rank()
+comm_size = MPI.COMM_WORLD.Get_size()
+
+print('Number of ranks {}, rank {}'.format(comm_size,my_rank))
