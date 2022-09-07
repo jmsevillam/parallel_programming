26ada843f2d525dab41ce4a42403ba679c8c13f0
Mauricio Sevilla <jmsevillam@unal.edu.co>
2022-09-07 19:31:56 +0200

    vector basic sum added

--- MPI/Basics_python/vector_basic.py
+++ MPI/Basics_python/vector_basic.py
@@ -4,15 +4,22 @@ my_rank = MPI.COMM_WORLD.Get_rank()
 comm_size = MPI.COMM_WORLD.Get_size()
 
 print('Number of ranks {}, rank {}'.format(comm_size,my_rank))
+N_points=1000000
+
+data = np.random.rand(N_points)
 
 
-data = np.random.rand(10)
+N_bins = 1000
+histo,_=np.histogram(data,bins=N_bins)
 
-result = np.zeros(10) 
-print(data[0])
+result = None
 
-MPI.COMM_WORLD.Reduce([data,MPI.DOUBLE],[result,MPI.DOUBLE],op = MPI.SUM,root = 0)
 if my_rank==0:
-	print(result)
+	result =  np.zeros(N_bins) 
 
+result=MPI.COMM_WORLD.reduce(histo,op = MPI.SUM,root = 0)
+
+if my_rank==0:
+	print(result/comm_size)
+	print(len(histo),len(result))
 
